9f0049be48b8f8ac77c6232b49c578f2bf624350
quyuanchao <quyuanchao@meituan.com>
2023-03-30 17:21:52 +0800

    新增 -postfix -replace

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,29 @@
 use std::env;
 use std::fs;
-use std::collections::HashMap;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let mut path = ".";
+    let mut path = "";
     let mut prefix = "";
+    let mut postfix = "";
+    let mut replace = "";
+    let mut replace_to = "";
     let mut new_name = "";
     let mut use_new_name = false;
     let mut show_help = false;
+    let mut need_replace = false;
 
     for i in 1..args.len() {
         if args[i] == "-path" {
             path = &args[i + 1];
         } else if args[i] == "-prefix" {
             prefix = &args[i + 1];
+        } else if args[i] == "-postfix" {
+            postfix = &args[i + 1];
+        } else if args[i] == "-replace" {
+            replace = &args[i + 1];
+            replace_to = &args[i + 2];
+            need_replace = true;
         } else if args[i] == "-new" {
             new_name = &args[i + 1];
             use_new_name = true;
@@ -23,18 +32,18 @@ fn main() {
         }
     }
 
-    if path == "." {
+    if path == "" {
         println!("The -path parameter is required.");
         return;
     }
-
     if show_help {
-        println!("Usage: ./path/to/program [-path <path>] [-prefix <prefix>] [-new <new_name>] [-help]");
-        println!("-path: the path to the directory containing the files to be renamed (default: current directory)");
+        println!("Usage: ./path/to/program [-path <path>] [-prefix <prefix>] [-new <new_name>] [-replace <old> <new>] [-postfix <postfix>] [-help]");
+        println!("-path: the path to the directory containing the files to be renamed (default: none)");
         println!("-prefix: the prefix to be added to the beginning of each file name (default: none)");
         println!("-new: the new name to be used for all files (default: none)");
+        println!("-replace: replace all occurrences of <old> with <new> in the file names (default: none)");
+        println!("-postfix: the postfix to be added to the end of each file name (default: none)");
         println!("-help: show this help message");
-        return;
     }
     let files = fs::read_dir(path).unwrap();
     let mut count = 1;
@@ -42,8 +51,13 @@ fn main() {
         let file_name = file.unwrap().file_name().into_string().unwrap();
         let new_file_name = if use_new_name {
             format!("{}{}.{}", new_name, count, file_name.split('.').last().unwrap())
+        } else if need_replace {
+            let file_name = file_name.replace(replace, replace_to);
+            let x: Vec<&str> = file_name.split(".").collect();
+            format!("{}{}{}.{}", prefix, x[0], postfix, x[1])
         } else {
-            format!("{}{}", prefix, file_name)
+            let x: Vec<&str> = file_name.split(".").collect();
+            format!("{}{}{}.{}", prefix, x[0], postfix, x[1])
         };
         fs::rename(format!("{}/{}", path, file_name), format!("{}/{}", path, new_file_name)).unwrap();
         count += 1;
