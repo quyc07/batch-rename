ce86db21b2728b7545d73ceea2b7cd84879bd359
quyc <quyc07@163.com>
2023-04-01 00:08:04 +0800

    简单重构下

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,14 @@
-use std::collections::HashSet;
 use std::env;
 use std::fs;
 
+use crate::param_parser::ParamParser;
+
+mod param_parser;
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let mut path = String::new();
-    let mut prefix = "";
-    let mut postfix = "";
-    let mut replace = "";
-    let mut replace_to = "";
-    let mut new_name = "";
-    let mut use_new_name = false;
-    let mut show_help = false;
-    let mut need_replace = false;
+    let br_args = ParamParser::parse_args(env::args());
 
-    let mut options_exist = HashSet::new();
-
-    for i in 1..args.len() {
-        if args[i] == "--path" {
-            check_option_exist(&mut options_exist, &args[i]);
-            path = args[i + 1].clone();
-            if !path.ends_with("/") {
-                path.push_str("/");
-            }
-        } else if args[i] == "--prefix" {
-            check_option_exist(&mut options_exist, &args[i]);
-            prefix = &args[i + 1];
-        } else if args[i] == "--postfix" {
-            check_option_exist(&mut options_exist, &args[i]);
-            postfix = &args[i + 1];
-        } else if args[i] == "--replace" {
-            check_option_exist(&mut options_exist, &args[i]);
-            replace = &args[i + 1];
-            replace_to = &args[i + 2];
-            need_replace = true;
-        } else if args[i] == "--new" {
-            check_option_exist(&mut options_exist, &args[i]);
-            new_name = &args[i + 1];
-            use_new_name = true;
-        } else if args[i] == "--help" {
-            show_help = true;
-        }
-    }
-
-    if path == "" && !show_help {
-        println!("The --path parameter is required.");
-        return;
-    }
-
-    if show_help {
+    if br_args.show_help {
         println!("Usage: batch_rename [OPTIONS]\n");
         println!("Options:");
         println!(
@@ -64,39 +24,40 @@ fn main() {
         return;
     }
 
-    let files = fs::read_dir(&path).unwrap();
+    if br_args.show_version {
+        println!("version: {}", br_args.version);
+        return;
+    }
+
+    if br_args.path == None {
+        println!("The --path parameter is required.");
+        return;
+    }
+
+    let files = fs::read_dir(&br_args.path.as_ref().unwrap()).unwrap();
     let mut count = 1;
     for file in files {
         let file_name = file.unwrap().file_name().into_string().unwrap();
-        let new_file_name = if use_new_name {
+        let new_file_name = if br_args.use_new_name {
             format!(
                 "{}{}.{}",
-                new_name,
+                br_args.new_name.as_ref().unwrap(),
                 count,
                 file_name.split('.').last().unwrap()
             )
-        } else if need_replace {
-            let file_name = file_name.replace(replace, replace_to);
+        } else if br_args.need_replace {
+            let file_name = file_name.replace(br_args.old.as_ref().unwrap().as_str(), br_args.new.as_ref().unwrap().as_str());
             let x: Vec<&str> = file_name.split(".").collect();
-            format!("{}{}{}.{}", prefix, x[0], postfix, x[1])
+            format!("{}{}{}.{}", br_args.prefix.as_ref().unwrap_or(&String::from("")), x[0], br_args.suffix.as_ref().unwrap_or(&String::from("")), x[1])
         } else {
             let x: Vec<&str> = file_name.split(".").collect();
-            format!("{}{}{}.{}", prefix, x[0], postfix, x[1])
+            format!("{}{}{}.{}", br_args.prefix.as_ref().unwrap_or(&String::from("")), x[0], br_args.suffix.as_ref().unwrap_or(&String::from("")), x[1])
         };
         fs::rename(
-            format!("{}/{}", path, file_name),
-            format!("{}/{}", path, new_file_name),
+            format!("{}/{}", br_args.path.as_ref().unwrap(), file_name),
+            format!("{}/{}", br_args.path.as_ref().unwrap(), new_file_name),
         )
-        .unwrap();
+            .unwrap();
         count += 1;
-    }
-}
-
-fn check_option_exist<'a>(options_exist: &mut HashSet<&'a String>, option: &'a String) {
-    if !options_exist.insert(option) {
-        panic!(
-            "Option {} has been set more than ones, please check the options",
-            option
-        );
     }
 }
---
+++ src/param_parser.rs
@@ -0,0 +1,69 @@
+use std::collections::HashSet;
+use std::env::Args;
+
+pub struct ParamParser;
+
+impl ParamParser {
+    pub fn parse_args(args: Args) -> BrArgs {
+        let args: Vec<String> = args.collect();
+        let mut br_args = BrArgs {
+            ..Default::default()
+        };
+        let mut options_exist = HashSet::new();
+        for i in 1..args.len() {
+            if args[i] == "--path" {
+                check_option_exist(&mut options_exist, &args[i]);
+                let mut path = args[i + 1].clone();
+                if !path.ends_with("/") {
+                    path.push_str("/");
+                    br_args.path = Some(path);
+                }
+            } else if args[i] == "--prefix" {
+                check_option_exist(&mut options_exist, &args[i]);
+                br_args.prefix = Some(args[i + 1].clone());
+            } else if args[i] == "--postfix" {
+                check_option_exist(&mut options_exist, &args[i]);
+                br_args.suffix = Some(args[i + 1].clone());
+            } else if args[i] == "--replace" {
+                check_option_exist(&mut options_exist, &args[i]);
+                br_args.old = Some(args[i + 1].clone());
+                br_args.new = Some(args[i + 2].clone());
+                br_args.need_replace = true;
+            } else if args[i] == "--new" {
+                check_option_exist(&mut options_exist, &args[i]);
+                br_args.new_name = Some(args[i + 1].clone());
+                br_args.use_new_name = true;
+            } else if args[i] == "--help" {
+                br_args.show_help = true;
+            } else if args[i] == "--version" {
+                br_args.version = include_str!("version.txt").to_string();
+                br_args.show_version = true;
+            }
+        }
+        br_args
+    }
+}
+
+fn check_option_exist<'a>(options_exist: &mut HashSet<&'a String>, option: &'a String) {
+    if !options_exist.insert(option) {
+        panic!(
+            "Option {} has been set more than ones, please check the options",
+            option
+        );
+    }
+}
+
+#[derive(Default, Debug)]
+pub struct BrArgs {
+    pub path: Option<String>,
+    pub prefix: Option<String>,
+    pub suffix: Option<String>,
+    pub old: Option<String>,
+    pub new: Option<String>,
+    pub new_name: Option<String>,
+    pub use_new_name: bool,
+    pub show_help: bool,
+    pub show_version: bool,
+    pub need_replace: bool,
+    pub version: String,
+}
